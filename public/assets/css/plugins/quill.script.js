6dafb47fec125f9dcc43311531b549d09015eaf6
Industrymall <Industrymall.net@gmail.com>
2023-12-29 07:49:46 +0000

    nabeel1st

---
+++ public/assets/css/plugins/quill.script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+$(document).ready(function () {
+  var quill1 = new Quill('#full-editor', {
+    modules: {
+      syntax: !0,
+      toolbar: [[{
+        font: []
+      }, {
+        size: []
+      }], ["bold", "italic", "underline", "strike"], [{
+        color: []
+      }, {
+        background: []
+      }], [{
+        script: "super"
+      }, {
+        script: "sub"
+      }], [{
+        header: "1"
+      }, {
+        header: "2"
+      }, "blockquote", "code-block"], [{
+        list: "ordered"
+      }, {
+        list: "bullet"
+      }, {
+        indent: "-1"
+      }, {
+        indent: "+1"
+      }], ["direction", {
+        align: []
+      }], ["link", "image", "video", "formula"], ["clean"]]
+    },
+    theme: 'snow'
+
+
+  });
+
+  quill1.on('text-change', function () {
+    document.getElementById('quill_content').value = quill.root.innerHTML;
+  });
+
+  var quill = new Quill('#snow-editor', {
+    modules: {
+      syntax: !0
+    },
+    theme: 'snow'
+  });
+  var quill = new Quill('#bubble-editor', {
+    modules: {
+      syntax: !0
+    },
+    theme: 'bubble'
+  });
+});
